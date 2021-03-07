88f3d255ccc532026ace5205f56025be62825068
Bruno Martins <bruno.martins217@gmail.com>
2021-03-07 12:07:36 -0300

    :package: Adicionando Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,5 +6,10 @@ export const requestAPI = () => ({ type: REQUEST_API });
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
-  // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {
+    dispatch(requestAPI());
+    const response = await fetch('https://aws.random.cat/meow')
+    const data = await response.json()
+    dispatch(getPicture(data))
+  }
 }
